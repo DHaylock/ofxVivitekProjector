23c5ceed9a65527d83fde5ef190a49af19eeb47e
David Haylock <david.haylock@watershed.co.uk>
2015-09-02 21:45:57 +0100

    - Thanks to Stewart Morgan for fixing the freezing serial port. - Implemented return value code

--- src/ofxVivatekProjector.cpp
+++ src/ofxVivatekProjector.cpp
@@ -1,6 +1,6 @@
 //--------------------------------------------------------------
 //* Name: ofxVivatekProjector.cpp
-//* Author: David Haylock
+//* Author: David Haylock, Stewart Morgan
 //* Creation Date: 17-08-2015
 //--------------------------------------------------------------
 #include "ofxVivatekProjector.h"
@@ -8,59 +8,170 @@
 //--------------------------------------------------------------
 void ofxVivatekProjector::openConnectionToProjector(string serialName,int baud)
 {
-    ofSerial::setup(serialName, baud);
-    ofSerial::listDevices();
+    currentSerialName = serialName;
+    currentBaud = baud;
+    projectorPort.listDevices();
+    projectorPort.setup(serialName, baud);
 }
 //--------------------------------------------------------------
 void ofxVivatekProjector::close()
 {
-    ofSerial::close();
+    projectorPort.flush();
+    projectorPort.drain();
+    projectorPort.close();
+}
+//--------------------------------------------------------------
+void ofxVivatekProjector::reconnect() {
+    if(projectorPort.isInitialized()) {
+        cout << "Reconnecting to projector..." << endl;
+        projectorPort.close();
+        projectorPort.setup(currentSerialName, currentBaud);
+    }
 }
 //--------------------------------------------------------------
 bool ofxVivatekProjector::isConnected()
 {
-    return ofSerial::isInitialized();
+    return projectorPort.isInitialized();
 }
 //--------------------------------------------------------------
-void ofxVivatekProjector::turnProjectorOn()
+bool ofxVivatekProjector::doCommand(const char *command, unsigned int command_length, unsigned char *reply, unsigned int reply_size)
 {
-    unsigned char command[COMMAND_LENGTH] = POWER_ON;
-    ofSerial::writeBytes(command,COMMAND_LENGTH);
-    cout << "Projector is Turning On " << POWER_ON << endl;
+    bool            free_reply = false;
+    bool            return_value = false;
+    unsigned int    reply_length = 0;
+    unsigned int    timeout = TIMEOUT_DURATION;
+
+    if(reply == NULL && reply_size == 0) {
+        free_reply = true;
+        reply_size = 1024;
+        reply = (unsigned char*)malloc(reply_size);
+    
+    } else if(reply_size == 0) {
+        cout << "Projector reply buffer size cannot be zero!" << endl;
+        return false;
+
+    } else {
+        free_reply = false;
+        memset(reply, 0, reply_size);
+    }
+        
+    // Drop off the trailing NULL
+    command_length--;
+    
+    cout << "Sending " << command_length << " bytes to the projector: " << command << endl;
+    projectorPort.writeBytes((unsigned char *)command, command_length);
+
+    // Give the projector a quarter of a second to reply
+    usleep(TIMEOUT_STEP_DURATION);
+    
+    while(timeout != 0) {
+        switch(int byte = projectorPort.readByte()) {
+        
+            case OF_SERIAL_NO_DATA:
+                cout << "Projector has no data; waiting (" << timeout << " retries remaining)..." << endl;
+                timeout--;
+                usleep(TIMEOUT_STEP_DURATION);
+                continue;
+
+            case OF_SERIAL_ERROR:
+                cout << "Projector returned error" << endl;
+                return false;
+
+            case '\r':
+                cout << "Projector replied: " << reply << endl;
+                projectorPort.drain();
+                return_value = (reply_length > 0) && (reply[0] == 'P');
+                if(free_reply) {
+                    free(reply);
+                }
+                return return_value;
+
+            default:
+                if(reply_length == reply_size) {
+                    cout << "Read too much from the projector for the buffer!!" << endl;
+                    return false;
+                }
+
+                //cout << "Projector sent: " << byte << " AKA " << (unsigned char)byte << endl;
+                reply[ reply_length++ ] = (unsigned char)byte;
+                break;
+        }
+    }
+
+    cout << "Timed-out waiting for data from the projector!" << endl;
+    if(free_reply) {
+        free(reply);
+    }
+    return false;
 }
 //--------------------------------------------------------------
-void ofxVivatekProjector::turnProjectorOff()
+bool ofxVivatekProjector::setProjectionMode(int mode)
 {
-    unsigned char command[COMMAND_LENGTH] = POWER_OFF;
-    ofSerial::writeBytes(command,COMMAND_LENGTH);
-    cout << "Projector is Turning Off " << POWER_OFF << endl;
+    switch(mode) {
+        case 0:
+            cout << "Projecting in Front Mode..." << endl;
+            return doCommand(COMMAND_MODE_FRONT, sizeof(COMMAND_MODE_FRONT));
+
+        case 1:
+            cout << "Projecting in Rear Mode..." << endl;
+            return doCommand(COMMAND_MODE_REAR, sizeof(COMMAND_MODE_REAR));
+
+        case 2:
+            cout << "Projecting in Ceiling Mode..." << endl;
+            return doCommand(COMMAND_MODE_FRONT_CEILING, sizeof(COMMAND_MODE_FRONT_CEILING));
+            
+        case 3:
+            cout << "Projecting in Rear/Ceiling Mode..." << endl;
+            return doCommand(COMMAND_MODE_REAR_CEILING, sizeof(COMMAND_MODE_REAR_CEILING));
+            
+        default:
+            cout << "Unknown mode to project in!" << endl;
+            return false;
+    }
+}
+//--------------------------------------------------------------
+bool ofxVivatekProjector::resetProjector()
+{
+    cout << "Projector is resetting..." << endl;
+    return doCommand(COMMAND_SYSTEM_RESET, sizeof(COMMAND_SYSTEM_RESET));
+}
+
+//--------------------------------------------------------------
+bool ofxVivatekProjector::resyncProjector()
+{
+    cout << "Projector is resyncing..." << endl;
+    return doCommand(COMMAND_RESYNC, sizeof(COMMAND_RESYNC));
+}
+
+//--------------------------------------------------------------
+bool ofxVivatekProjector::turnProjectorOn()
+{
+    cout << "Projector is Turning On..." << endl;
+    return doCommand(COMMAND_POWER_ON, sizeof(COMMAND_POWER_ON));
+}
+//--------------------------------------------------------------
+bool ofxVivatekProjector::turnProjectorOff()
+{
+    cout << "Projector is Turning Off..." << endl;
+    return doCommand(COMMAND_POWER_OFF, sizeof(COMMAND_POWER_OFF));
 }
 //--------------------------------------------------------------
 string ofxVivatekProjector::getProjectorStatus()
 {
-    unsigned char command[COMMAND_LENGTH] = SYSTEM_STATUS;
-    ofSerial::writeBytes(command,COMMAND_LENGTH);
-    cout << "Getting System Status " << endl;
-    
-    unsigned char bytesReturned[1];
-    memset(bytesReturned,0,1);
-    ofSerial::readBytes(bytesReturned,1);
-    
-    switch (bytesReturned[0]) {
-        case '0':
-            return "Projector is Resetting";
-            break;
-        case '1':
-            return "Projector is On Standby";
-            break;
-        case '2':
-            return "Projector is Operating";
-            break;
-        case '3':
-            return "Projector is Cooling";
-            break;
-        default:
-            break;
+    cout << "Getting System Status..." << endl;
+    unsigned char reply[1024];
+    if(doCommand(COMMAND_SYSTEM_STATUS, sizeof(COMMAND_SYSTEM_STATUS), reply, sizeof(reply))) {
+        return ofToString(reply);
     }
-    
+    return "";
+}
+//--------------------------------------------------------------
+int ofxVivatekProjector::getLampHours()
+{
+    cout << "Getting lmap-hours..." << endl;
+    unsigned char reply[1024];
+    if(doCommand(COMMAND_GET_LAMP_HOURS, sizeof(COMMAND_GET_LAMP_HOURS), reply, sizeof(reply))) {
+        return (int)std::strtol((const char *)(reply + 1), (char **)NULL, 10);
+    }
+    return -1;
 }
--- src/ofxVivatekProjector.h
+++ src/ofxVivatekProjector.h
@@ -1,31 +1,57 @@
 //--------------------------------------------------------------
 //* Name: ofxVivatekProjector.h
-//* Author: David Haylock
+//* Author: David Haylock, Stewart Morgan
 //* Creation Date: 17-08-2015
 //--------------------------------------------------------------
 #include "ofMain.h"
+#include <stdlib.h>
+
+#define TIMEOUT_STEP_DURATION      250000   // In uSeconds
+#define TIMEOUT_DURATION           20       // In iterations of TIMEOUT_STEP_DURATION, 20 ~5s
+
+#define COMMAND_POWER_ON            "V99S0001\r"
+#define COMMAND_POWER_OFF           "V99S0002\r"
+#define COMMAND_RESYNC              "V99S0003\r"
+#define COMMAND_GET_LAMP_HOURS      "V99G0004\r"
+#define COMMAND_SYSTEM_RESET        "V99S0006\r"
+#define COMMAND_SYSTEM_STATUS       "V99G0007\r"
+
+#define COMMAND_MODE_FRONT          "V99S03080\r"
+#define COMMAND_MODE_REAR           "V99S03081\r"
+#define COMMAND_MODE_FRONT_CEILING  "V99S03082\r"
+#define COMMAND_MODE_REAR_CEILING   "V99S03083\r"
 
 
-#define POWER_ON "VXXS0001\r"
-#define POWER_OFF "VXXS0002\r"
-#define SYSTEM_STATUS "VXXS0007\r"
+#define NUM_BYTES 1
 
-#define COMMAND_LENGTH 10
 #define BAUD 9600
 
-class ofxVivatekProjector : public ofSerial {
+class ofxVivatekProjector {
     public:
         void openConnectionToProjector(string serialName,int baud = BAUD);
         void close();
     
         bool isConnected();
+        void reconnect();
+        bool doCommand(const char *command, unsigned int command_length, unsigned char *reply = NULL, unsigned int reply_size = 0);
     
-        void turnProjectorOn();
-        void turnProjectorOff();
+        bool resyncProjector();
+        bool resetProjector();
+        bool turnProjectorOn();
+        bool turnProjectorOff();
+        bool setProjectionMode(int mode);
+    
         string getProjectorStatus();
+        int getLampHours();
     
     protected:
-    
+        unsigned char returnValue;
+        ofSerial projectorPort;
+        unsigned char	bytesReturned[NUM_BYTES];
+        string  messageBuffer;
+        string message;
     
     private:
+        string currentSerialName;
+        int currentBaud;
 };
