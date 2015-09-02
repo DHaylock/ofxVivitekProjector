12636147e10dc0da67bf1907f00bb0bb9e91f4da
David Haylock <david.haylock@watershed.co.uk>
2015-09-02 21:44:48 +0100

    - Further updates to example

--- simple_example/src/ofApp.cpp
+++ simple_example/src/ofApp.cpp
@@ -1,3 +1,8 @@
+//--------------------------------------------------------------
+//* Name: ofApp.cpp
+//* Author: David Haylock, Stewart Morgan
+//* Creation Date: 17-08-2015
+//--------------------------------------------------------------
 #include "ofApp.h"
 
 //--------------------------------------------------------------
@@ -5,7 +10,7 @@ void ofApp::setup()
 {
     ofBackground(0, 0, 0);
     title.loadFont("mono.ttf",15,true,false,false,0.3,75);
-    projector.openConnectionToProjector("thisport");
+    projector.openConnectionToProjector("/dev/tty.usbserial");
 }
 //--------------------------------------------------------------
 void ofApp::update()
@@ -26,17 +31,81 @@ void ofApp::draw()
 //--------------------------------------------------------------
 void ofApp::exit()
 {
-    projector.turnProjectorOff();
+//    projector.turnProjectorOff();
     projector.close();
 }
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key)
 {
-    if (key == 'o') {
-        projector.turnProjectorOn();
-    }
-    if (key == 'c') {
-        projector.turnProjectorOff();
+    string status;
+    int lamp_hours;
+    
+    switch (key) {
+        case '0':
+            projector.setProjectionMode(0);
+            break;
+        case '1':
+            projector.setProjectionMode(1);
+            break;
+        case '2':
+            projector.setProjectionMode(2);
+            break;
+        case '3':
+            projector.setProjectionMode(3);
+            break;
+
+        case 'c':
+            projector.turnProjectorOff();
+            break;
+            
+        case 'g':
+            status = projector.getProjectorStatus();
+            if(status == "") {
+                cout << "Failed to get projector status" << endl;
+            } else {
+                cout << "Projector status is currently: " << status << endl;
+            }
+            break;
+            
+        case 'l':
+            lamp_hours = projector.getLampHours();
+            if(lamp_hours < 0) {
+                cout << "Failed to get projector lamp-hours" << endl;
+            } else {
+                cout << "Projector lamp-hours is currently: " << lamp_hours << endl;
+            }
+            break;
+            
+        case 'o':
+            projector.turnProjectorOn();
+            break;
+            
+        case 'r':
+            projector.resetProjector();
+            break;
+            
+        case 'R':
+            projector.reconnect();
+            break;
+
+        case 's':
+            projector.resyncProjector();
+            break;
+            
+        case 'h':
+            cout << "Help:" << endl;
+            cout << "c: Turn Off" << endl;
+            cout << "g: Get Current status" << endl;
+            cout << "l: Get Lamp hours" << endl;
+            cout << "o: Turn On " << endl;
+            cout << "r: Reset Projector" << endl;
+            cout << "R: Reconnect serial link" << endl;
+            cout << "s: Resync Projector" << endl;
+            cout << "h: This help screen" << endl;
+            break;
+
+        default:
+            break;
     }
 }
 //--------------------------------------------------------------
